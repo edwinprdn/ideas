b35bdd5757335c35894e93d36c4250c24978186a
edwinprdn <edwinpradana41@student.untan.ac.id>
2024-02-26 13:38:38 +0700

    Follow and Unfollow

---
+++ app/Http/Controllers/FollowerController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+
+class FollowerController extends Controller
+{
+    public function follow(User $user)
+    {
+        $follower = auth()->user();
+        $follower->followings()->attach($user);
+
+        return redirect()->route('users.show', $user->id)->with('success', 'You are now following ' . $user->name);
+    }
+
+    public function unfollow(User $user)
+    {
+        $follower = auth()->user();
+        $follower->followings()->detach($user);
+
+        return redirect()->route('users.show', $user->id)->with('success', 'You are no longer following ' . $user->name);
+    }
+}
